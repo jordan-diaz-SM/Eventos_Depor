6dd4d5a1e4ef8889cef8602accac68b0462232e2
jordan-diaz <jdiapine@everis.com>
2021-11-09 23:24:50 -0500

    cambios F

--- src/Modelos/EventoDeportivo.java
+++ src/Modelos/EventoDeportivo.java
@@ -2,5 +2,10 @@ package Modelos;
 
 public class EventoDeportivo {
     private String CodigoEvento;
-    
+    private String Fecha;
+    private  String EquipoLocal;
+    private String EquipoVisita;
+    private String Resultado;
+    private String  
+
 }
